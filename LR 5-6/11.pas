76e6416b0a447a034018e59fda6f26ef2bf8c205
pitocheck <117996315+pitocheck@users.noreply.github.com>
2022-11-11 17:03:49 +0300

    Add files via upload

---
+++ LR 5-6/11.pas
@@ -0,0 +1,20 @@
+﻿var i, i2, i3, l, min: integer;
+a:array[1..20] of integer;
+begin
+  randomize;
+  for i:=1 to 20 do begin
+    A[i]:=random(100)-25;
+    if (A[i] mod 2 = 0) and (l <> 1) then
+    begin
+      inc(l);
+      i3:=i;
+      writeln('первое четн',' ',A[i3]);
+    end;
+  end;
+  for i:=1 to 20 do
+    if A[i] mod 2 = 0 then
+      inc(i2);
+  A[i3+1]:=i2;
+  writeln('колво четн',' ',A[i2]);
+  writeln('итог масс',' ',a);
+end. 
